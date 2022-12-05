b659d91bf7bf028655b3409b9065e4c5cd95ffb5
csk6314 <37896060+csk6314@users.noreply.github.com>
2022-12-06 00:00:30 +0900

    Login Page UI

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import './index.css'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div id="container-Login">
+      <div id="form-Login">
+        <span>와쓔</span>
+        <div>
+          <span>아이디</span>
+          <input />
+        </div>
+        <div>
+          <span>비밀번호</span>
+          <input type="password" />
+        </div>
+        <Link to="/main" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+        <button>로그인</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
