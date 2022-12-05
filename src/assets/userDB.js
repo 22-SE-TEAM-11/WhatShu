173e03b001855f410a7a6a88b0d702eb811494fc
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2022-12-06 02:44:42 +0900

    ✨ 마커 여러개 기능 (#4)
    * :sparkles: create map
    
    * :sparkles: get my coordinate
    
    * :coffin: delete hook
    
    * :label: 주석 추가
    
    * :sparkles: 마커 만듦.
    
    * :sparkles: on off 처리

---
+++ src/assets/userDB.js
@@ -0,0 +1,20 @@
+export const userDB = [
+  {
+    id: "rjsdn0124",
+    password: "tmdrbajfl123",
+    role: "0",
+    foodtruck: [],
+  },
+  {
+    id: "admin1111",
+    password: "admin1111",
+    role: "1",
+    foodtruck: ["1"],
+  },
+  {
+    id: "admin1234",
+    password: "admin1234",
+    role: "1",
+    foodtruck: ["2"],
+  },
+];
--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,26 +1,48 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { truckDB } from "../../assets/truckDB";
 
 const { kakao } = window;
 
-function KakaoMapScript(lat = 33.450701, lng = 126.570667) {
+function KakaoMapScript(lat = 33.450701, lng = 126.570667, location) {
   const container = document.getElementById("map");
   const options = {
     center: new kakao.maps.LatLng(lat, lng),
     level: 2,
   };
+  const map = new kakao.maps.Map(container, options);
 
   // 내 위치에 마커를 생성합니다
   var imageSize = new kakao.maps.Size(24, 35);
-  var my_loc_image =
-    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-  var my_loc_marker = new kakao.maps.MarkerImage(my_loc_image, imageSize);
+  var my_loc_marker = new kakao.maps.MarkerImage(
+    "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+    imageSize
+  );
   var my_marker_position = new kakao.maps.LatLng(lat, lng);
   var my_marker = new kakao.maps.Marker({
     position: my_marker_position,
     image: my_loc_marker,
   });
-  const map = new kakao.maps.Map(container, options);
+
+  let truck;
+  for (var i = 0; i < truckDB.length; i++) {
+    truck = truckDB[i];
+    if (!truck.on && truck.categories !== location.state.category) continue;
+    // 마커 이미지를 생성합니다
+    var truck_image = new kakao.maps.MarkerImage(
+      "https://cdn-icons-png.flaticon.com/512/1046/1046762.png",
+      imageSize
+    );
+
+    // 마커를 생성합니다
+    var truck_marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: new kakao.maps.LatLng(truck.latitudee, truck.longitude), // 마커를 표시할 위치
+      title: truckDB[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      image: truck_image, // 마커 이미지
+    });
+    truck_marker.setMap(map);
+  }
 
   // 마커가 지도 위에 표시되도록 설정합니다
   my_marker.setMap(map);
@@ -30,7 +52,12 @@ const MenuPage = () => {
   const location = useLocation();
   if ("geolocation" in navigator)
     navigator.geolocation.getCurrentPosition((position) => {
-      KakaoMapScript(position.coords.latitude, position.coords.longitude);
+      console.log(position);
+      KakaoMapScript(
+        position.coords.latitude,
+        position.coords.longitude,
+        location
+      );
     });
 
   return (
