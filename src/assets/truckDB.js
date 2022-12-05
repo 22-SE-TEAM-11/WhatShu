36b3b76dc3151ca77c4ad49e2a513cb6b4cb6a9f
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-12-06 02:27:08 +0900

    :sparkles: 마커 만듦.

---
+++ src/assets/truckDB.js
@@ -0,0 +1,34 @@
+export const truckDB = [
+  {
+    id: 1,
+    latitudee: "36.365",
+    longitude: "127.3446",
+    name: "takoyaki",
+    menu: ["takoyaki", "octopus"],
+    categories: "snack",
+  },
+  {
+    id: 2,
+    latitudee: "36.36258",
+    longitude: "127.3484",
+    name: "burger",
+    menu: ["burger", "cola"],
+    categories: "fastfood",
+  },
+  {
+    id: 3,
+    latitudee: "36.36278",
+    longitude: "127.35",
+    name: "fruit",
+    menu: ["apple", "귤"],
+    categories: "fruit",
+  },
+  {
+    id: 4,
+    latitudee: "36.3629",
+    longitude: "127.3473",
+    name: "수육",
+    menu: ["수육", "순대"],
+    categories: "jokbo",
+  },
+];
